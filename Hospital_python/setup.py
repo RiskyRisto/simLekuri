82cfb2fc49480455b78bce4a6beae6eed62a4733
Mika Sipilä <mikasip11@gmail.com>
2020-11-13 12:22:45 +0200

    Add setup and .gitignore

---
+++ Hospital_python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Hospital Simulation',
+      version='1.0',
+      description='Hospital Simulation',
+      author='Mika Sipilä',
+      author_email='mikasip11@gmail.com',
+      url='-',
+      install_requires=['simpy', 'random']
+     )
